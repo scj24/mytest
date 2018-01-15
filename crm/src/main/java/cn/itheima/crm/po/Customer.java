5ec3b975f6700b455ed33f282a55c971774361be
scj <m18873269373@163.com>
2018-01-16 00:11:24 +0800

    第一次提交

---
+++ crm/src/main/java/cn/itheima/crm/po/Customer.java
@@ -0,0 +1,180 @@
+package cn.itheima.crm.po;
+
+import java.util.Date;
+/**
+ * 客户pojo
+ *
+ */
+public class Customer {
+	
+	  private Long cust_id; 
+	  private String cust_name; 
+	  private Long cust_user_id; 
+	  private Long cust_create_id; 
+	  private String cust_source; 
+	  private String cust_industry; 
+	  private String cust_level; 
+	  private String cust_linkman; 
+	  private String cust_phone; 
+	  private String cust_mobile; 
+	  private String cust_zipcode; 
+	  private String cust_address; 
+	  private Date cust_createtime; 
+	/**
+	 * @return the cust_id
+	 */
+	public Long getCust_id() {
+		return cust_id;
+	}
+	/**
+	 * @param cust_id the cust_id to set
+	 */
+	public void setCust_id(Long cust_id) {
+		this.cust_id = cust_id;
+	}
+	/**
+	 * @return the cust_name
+	 */
+	public String getCust_name() {
+		return cust_name;
+	}
+	/**
+	 * @param cust_name the cust_name to set
+	 */
+	public void setCust_name(String cust_name) {
+		this.cust_name = cust_name;
+	}
+	/**
+	 * @return the cust_user_id
+	 */
+	public Long getCust_user_id() {
+		return cust_user_id;
+	}
+	/**
+	 * @param cust_user_id the cust_user_id to set
+	 */
+	public void setCust_user_id(Long cust_user_id) {
+		this.cust_user_id = cust_user_id;
+	}
+	/**
+	 * @return the cust_create_id
+	 */
+	public Long getCust_create_id() {
+		return cust_create_id;
+	}
+	/**
+	 * @param cust_create_id the cust_create_id to set
+	 */
+	public void setCust_create_id(Long cust_create_id) {
+		this.cust_create_id = cust_create_id;
+	}
+	/**
+	 * @return the cust_source
+	 */
+	public String getCust_source() {
+		return cust_source;
+	}
+	/**
+	 * @param cust_source the cust_source to set
+	 */
+	public void setCust_source(String cust_source) {
+		this.cust_source = cust_source;
+	}
+	/**
+	 * @return the cust_industry
+	 */
+	public String getCust_industry() {
+		return cust_industry;
+	}
+	/**
+	 * @param cust_industry the cust_industry to set
+	 */
+	public void setCust_industry(String cust_industry) {
+		this.cust_industry = cust_industry;
+	}
+	/**
+	 * @return the cust_level
+	 */
+	public String getCust_level() {
+		return cust_level;
+	}
+	/**
+	 * @param cust_level the cust_level to set
+	 */
+	public void setCust_level(String cust_level) {
+		this.cust_level = cust_level;
+	}
+	/**
+	 * @return the cust_linkman
+	 */
+	public String getCust_linkman() {
+		return cust_linkman;
+	}
+	/**
+	 * @param cust_linkman the cust_linkman to set
+	 */
+	public void setCust_linkman(String cust_linkman) {
+		this.cust_linkman = cust_linkman;
+	}
+	/**
+	 * @return the cust_phone
+	 */
+	public String getCust_phone() {
+		return cust_phone;
+	}
+	/**
+	 * @param cust_phone the cust_phone to set
+	 */
+	public void setCust_phone(String cust_phone) {
+		this.cust_phone = cust_phone;
+	}
+	/**
+	 * @return the cust_mobile
+	 */
+	public String getCust_mobile() {
+		return cust_mobile;
+	}
+	/**
+	 * @param cust_mobile the cust_mobile to set
+	 */
+	public void setCust_mobile(String cust_mobile) {
+		this.cust_mobile = cust_mobile;
+	}
+	/**
+	 * @return the cust_zipcode
+	 */
+	public String getCust_zipcode() {
+		return cust_zipcode;
+	}
+	/**
+	 * @param cust_zipcode the cust_zipcode to set
+	 */
+	public void setCust_zipcode(String cust_zipcode) {
+		this.cust_zipcode = cust_zipcode;
+	}
+	/**
+	 * @return the cust_address
+	 */
+	public String getCust_address() {
+		return cust_address;
+	}
+	/**
+	 * @param cust_address the cust_address to set
+	 */
+	public void setCust_address(String cust_address) {
+		this.cust_address = cust_address;
+	}
+	/**
+	 * @return the cust_createtime
+	 */
+	public Date getCust_createtime() {
+		return cust_createtime;
+	}
+	/**
+	 * @param cust_createtime the cust_createtime to set
+	 */
+	public void setCust_createtime(Date cust_createtime) {
+		this.cust_createtime = cust_createtime;
+	}
+
+}
